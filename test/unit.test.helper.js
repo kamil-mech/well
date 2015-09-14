7cca2efb67ca3d153f569260e58da3074fd2c42c
Kamil Mech <kamil.mech@nearform.com>
2015-09-14 01:05:46 +0100

    tidy up helper

--- test/unit.test.helper.js
+++ test/unit.test.helper.js
@@ -1,27 +1,27 @@
-// ADD COPYRIGHT INFO OR A DISCLAIMER
-
-var db = process.env['npm_config_db']
-var ip = process.env['npm_config_ip']
-var port = process.env['npm_config_port']
+"use strict";
 
 module.exports =
   function() {
 
+    var db = process.env['npm_config_db']
+    var ip = process.env['npm_config_ip']
+    var port = process.env['npm_config_port']
     var _  = require('lodash')
     var fs = require('fs')
-    var self = this;
+    var async = require('async')
+    var self = this
 
-    this.init_empty = function(handler, done) {
-      var si = require('seneca')({
-        errhandler: handler,
+    this.init_empty = function(errhandler, done) {
+      var seneca = require('seneca')({
+        errhandler: errhandler,
         default_plugins:{'mem-store':false}
       })
 
       var util = require('util')
 
       // get options
-      si.use('options', '../options.well.js')
-      var options = si.export('options')
+      seneca.use('options', '../options.well.js')
+      var options = seneca.export('options')
       options.dev_setup = options.well.dev_setup // <- this is not normal, check if app should really work like this
 
       // init and clean db
@@ -57,62 +57,60 @@ module.exports =
       }
       if (db_args.host && db_args.port) console.log('connecting at ' + db_args.host + ':' + db_args.port)
         else console.log('db connection is internal')
-      si.use(db, db_args)
+      seneca.use(db, db_args)
 
-      this.clean_db(si, function(err){
+      this.clean_db(seneca, function(err){
 
           // init well.js
-          si.use('user')
-          si.use('../well', options)
+          seneca.use('user')
+          seneca.use('../well', options)
 
-          done(si)
+          done(seneca)
       })
     }
 
-    this.init = function(handler, done) {
-var fid = (Math.random()* 1000).toString().substring(0, 3)
-      this.init_empty(handler, function(si){
+    this.init = function(errhandler, done) {
+      this.init_empty(errhandler, function(seneca){
 
       self.entities = {
-        event: si.make$('event'),
-        team: si.make$('team'),
-        user: si.make$('sys/user')
+        event: seneca.make$('event'),
+        team: seneca.make$('team'),
+        user: seneca.make$('sys/user')
       }
 
-      // add event A
-      ;si
-        .make$('event')
-        .make$(_.extend({
-          numcards: 52,
-          numteams: 2,
-          name: 'MeetupA',
-          code: 'ma',
-          users: {}
-        }, _.omit({
-          name: 'MeetupA',
-          code: 'ma'
-        }, ['role', 'cmd'])))
-        .save$(function(err, event_a){
+      var event_a = {}
+      var event_b = {}
 
-      // add event B
-      ;si
-        .make$('event')
-        .make$(_.extend({
-          numcards: 52,
-          numteams: 1,
-          name: 'MeetupB',
-          code: 'mb',
-          users: {}
-        }, _.omit({
-          name: 'MeetupB',
-          code: 'mb'
-        }, ['role', 'cmd'])))
-        .save$(function(err, event_b){
-
-      // add team Red to event A
-      ;si
-        .make$('team')
-        .make$({
+      // unfortunately seneca stores really don't like parallel
+      // add events
+      async.series([
+        function(cb) {
+          self.entities.event.save$({
+            numcards: 52,
+            numteams: 2,
+            name: 'MeetupA',
+            code: 'ma',
+            users: {}
+          }, function(err, res){
+            event_a = res
+            cb()
+          })
+        },
+        function(cb) {
+          self.entities.event.save$({
+            numcards: 52,
+            numteams: 1,
+            name: 'MeetupB',
+            code: 'mb',
+            users: {}
+          }, function(err, res){
+            event_b = res
+            cb()
+          })
+        },
+        // add teams
+        function(cb) {
+          self.entities.team.save$({
             num: 0,
             event: event_a.id,
             eventcode: event_a.code,
@@ -120,109 +118,61 @@ var fid = (Math.random()* 1000).toString().substring(0, 3)
             wells: {},
             numwells: 0,
             users: {}
-          })
-        .save$(function(err, team_r){
-
-      // add team Green to event A
-      ;si
-        .make$('team')
-        .make$({
-          num: 1,
-          event: event_a.id,
-          eventcode: event_a.code,
-          name: 'Green',
-          wells: {},
-          numwells: 0,
-          users: {}
-        })
-        .save$(function(err, team_g) {
-        
-      // add team Blue to event B
-      ;si
-      .make$('team')
-      .make$({
-        num: 0,
-        event: event_b.id,
-        eventcode: event_b.code,
-        name: 'Blue',
-        wells: {},
-        numwells: 0,
-        users: {}
-      })
-      .save$(function(err, team_b) {
+          }, cb)
+        },
+        function(cb) {
+          self.entities.team.save$({
+            num: 1,
+            event: event_a.id,
+            eventcode: event_a.code,
+            name: 'Green',
+            wells: {},
+            numwells: 0,
+            users: {}
+          }, cb)
+        },
+        function(cb) {
+          self.entities.team.save$({
+            num: 0,
+            event: event_b.id,
+            eventcode: event_b.code,
+            name: 'Blue',
+            wells: {},
+            numwells: 0,
+            users: {}
+          }, cb)
+        }
+      ], function(err, res){
      
      // add users
-      si.util.recurse(6, function( index, next ){
-        si
-        .act('role:user,cmd:register', {
-            nick: 'u' + index,
-            name: 'n' + index,
-            password: 'p' + index
-          }, next)
+      async.map([0, 1, 2, 3, 4, 5, 6], function(index, next){
+        seneca.act('role:user,cmd:register', {
+          nick: 'u' + index,
+          name: 'n' + index,
+          password: 'p' + index
+        }, next)
       }, function(err, data){
 
-          done(si)
-      }) }) }) }) }) }) })
+          done(seneca)
+      }) }) })
+    }
+
+    this.to_dbsc = function(seneca){
+
     }
 
     // erases all entities from db
     this.clean_db = function(seneca, cb){
-      erase('sys/user', seneca, function() {
-        erase('team', seneca, function() {
-          erase('event', seneca, function() {
-            if ('db' === 'jsonfile-store') seneca.make$('sys', 'entity').save$()
-            cb()
-          })
-        })
-      })
+      async.parallel([
+        erase.bind(null, 'sys/user', seneca),
+        erase.bind(null, 'team', seneca),
+        erase.bind(null, 'event', seneca),
+      ], cb)
     }
 
     // erase particular entity from db
-    function erase(entity, seneca, callback){
-      seneca.act({role:'entity', cmd:'remove', qent:seneca.make(entity), q:{all$ : true}}, function(err, data){
-        if (err) seneca.error(err)
-          callback(err)
-      })
+    function erase(entity, seneca, cb){
+      seneca.act({role:'entity', cmd:'remove', qent:seneca.make(entity), q:{all$ : true}}, cb)
     }
 
-    this.list_all = function (seneca) {
-      seneca.make$('event').list$(function(err, dblist) {
-        if (err) return console.error(err)
-        console.log("\n\t\t ---")
-        console.log("\t\tEvents\n")
-        dblist.forEach(function(element) {
-          console.log(element + '\n')
-        })
-        console.log("\n\t\t ---\n");
-      })
-
-      seneca.make$('team').list$(function(err, dblist) {
-        console.log("\n\t\t ---")
-        console.log("\t\tTeams\n")
-        if (err) return console.error(err)
-        dblist.forEach(function(element) {
-          console.log(element + '\n')
-        })
-        console.log("\n\t\t ---\n")
-      })
-
-      seneca.make$('sys/user').list$(function(err, dblist) {
-        console.log("\n\t\t ---")
-        console.log("\t\tUsers\n")
-        if (err) return console.error(err)
-        dblist.forEach(function(element) {
-          console.log(element + '\n')
-        })
-        console.log("\n\t\t ---\n")
-      })
-    }
-
-    this.show_commands = function (seneca) {
-      console.log("\n\t\t---")
-      console.log("\tSENECA COMMANDS AVAILABLE:\n")
-      seneca.list().forEach(function(element) {
-        console.log(element)
-      })
-      console.log("\n\t\t---\n")
-    }
   }
